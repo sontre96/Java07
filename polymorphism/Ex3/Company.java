d7fa3e68d22a35ec97d033a7bce3abc31cdd3528
sontre96 <sontre96@gmail.com>
2022-06-30 15:34:46 +0700

    update

--- polymorphism/Ex3/Company.java
+++
@@ -1,8 +0,0 @@
-package polymorphism.Ex3;
-
-public class Company extends Taxpayer {
-    public double pay(){
-        System.out.println("Company: to pay 1000");
-        return 1000;
-    }
-}
--- polymorphism/Ex3/FreeLand.java
+++
@@ -1,8 +0,0 @@
-package polymorphism.Ex3;
-
-public class FreeLand extends Taxpayer {
-    public double pay() {
-        System.out.println("FreeLand: to pay 10");
-        return 10;
-    }
-}
